b9d18254e573d3a77b7c3b3337af67c69c88b276
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-09-14 07:04:41 -0700

    specialization module completed with jUnit

---
+++ src/main/java/in/nareshit/raghu/service/impl/SpecializationServiceImpl.java
@@ -0,0 +1,54 @@
+package in.nareshit.raghu.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.repository.SpecializationRepository;
+import in.nareshit.raghu.service.ISpecializationService;
+
+@Service
+public class SpecializationServiceImpl implements ISpecializationService {
+
+	@Autowired
+	private SpecializationRepository repo;
+
+	@Override
+	public Long saveSpecialization(Specialization spec) {
+
+		return repo.save(spec).getId();
+	}
+
+	@Override
+	public List<Specialization> getAllSpecializations() {
+
+		return repo.findAll();
+	}
+
+	@Override
+	public void removeSpecialization(Long id) {
+		repo.deleteById(id);
+
+	}
+
+	@Override
+	public Specialization getOneSpecialization(Long id) {
+		Optional<Specialization> spec = repo.findById(id);
+		if (spec.isPresent()) {
+			return spec.get();
+		} else {
+			return null;
+		}
+
+	}
+
+	@Override
+	public void updateSpecialization(Specialization spec) {
+		repo.save(spec);
+
+	}
+
+}
---
+++ src/test/java/in/nareshit/raghu/specialization/SpecializationTest.java
@@ -0,0 +1,50 @@
+package in.nareshit.raghu.specialization;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.repository.SpecializationRepository;
+
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestMethodOrder(OrderAnnotation.class)
+public class SpecializationTest {
+
+	private static Specialization spec;
+
+	@Autowired
+	private SpecializationRepository repo;
+
+	@BeforeAll
+	public static void init() {
+		spec = new Specialization(null, "4", "Family Physicians",
+				"They care for the whole family, including children, adults, and the elderly.");
+	}
+
+	@Test
+	@Order(1)
+	public void testSpecCreate() {
+		assertNotNull(spec, "spec is not initialized");
+		Long id = repo.save(spec).getId();
+		assertNotNull(id, "spec is not saved to DB");
+	}
+
+	@AfterAll
+	public static void clean() {
+		spec = null;
+	}
+
+}
