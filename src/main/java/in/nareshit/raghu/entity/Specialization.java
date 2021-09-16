85cf4d55d9896683eb2c575f30aaf83ef559ecc9
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2021-09-16 17:47:22 +0530

    @id added to specialization entity

--- src/main/java/in/nareshit/raghu/entity/Specialization.java
+++ src/main/java/in/nareshit/raghu/entity/Specialization.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "specialization_tab")
 public class Specialization {
-
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "spec_id_col")
 	private Long id;
