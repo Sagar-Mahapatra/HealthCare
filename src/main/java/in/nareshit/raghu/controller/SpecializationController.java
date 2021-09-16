c2f13b4e8f2047f505211647e81374bb633fb940
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2021-09-16 17:33:28 +0530

    updated till 15-09-2021

--- src/main/java/in/nareshit/raghu/controller/SpecializationController.java
+++ src/main/java/in/nareshit/raghu/controller/SpecializationController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import in.nareshit.raghu.entity.Specialization;
 import in.nareshit.raghu.service.ISpecializationService;
@@ -18,11 +22,39 @@ public class SpecializationController {
 	@Autowired
 	private ISpecializationService service;
 
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "specializationRegister";
+
+	}
+
 	@GetMapping("/all")
-	public String viewAll(Model model) {
+	public String viewAll(Model model, @RequestParam(value = "message", required = false) String message) {
 		List<Specialization> list = service.getAllSpecializations();
 		model.addAttribute("list", list);
+		model.addAttribute("message", message);
 		return "SpecializationData";
+	}
+
+	@GetMapping("/delete")
+	public String deleteData(@RequestParam Long id, RedirectAttributes attributes) {
+		service.removeSpecialization(id);
+		attributes.addAttribute("message", "Record (" + id + ") is removed");
+		return "redirect:all";
+	}
+
+	@GetMapping("/edit")
+	public String showEditPage(@RequestParam Long id, Model model) {
+		Specialization spec = service.getOneSpecialization(id);
+		model.addAttribute("specialization", spec);
+		return "SpecializationEdit";
+	}
+
+	@PostMapping("/update")
+	public String updateData(@ModelAttribute Specialization specialization, RedirectAttributes attributes) {
+		service.updateSpecialization(specialization);
+		attributes.addAttribute("message", "Record (" + specialization.getId() + ") is updated");
+		return "redirect:all";
 	}
 
 }
